387147cddf04bec2109665bdef66294987835732
Prabhath <96005302+prabhath-kj@users.noreply.github.com>
2022-02-17 15:34:14 +0530

    scripts.js updated

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Freelancer v7.0.5 (https://startbootstrap.com/theme/freelancer)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
